bddabff45d7e084c1a5ae7caab5a217c19cfaafd
blur <damienhebert710@gmail.com>
2022-05-22 23:23:48 -0500

    added names for values table

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,7 +12,7 @@ VALUES ('Chief Executive Officer', 350000, 3),
 ('Chief Operating Officer', 300000, 3)
 ('IT Project Manager', 750000, 4)
 ('IT Project Director', 120000, 4)
-('HR Director', 250000, 5)
+('HR Director', 150000, 5)
 ('HR Generalist', 70000, 5)
 ('HR Coordinator', 65000, 5)
 ('Sr.Developer', 175000, 2)
@@ -22,4 +22,24 @@ VALUES ('Chief Executive Officer', 350000, 3),
 ('Account Executive', 175000, 1);
 
 
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Edward', 'Artmore', 11, 1), 
+('Dexter', 'Tomball', 13, 2), 
+('Margaret', 'Spectre', 1, 4),
+('Tony', 'Stark', 4, 2),
+('Bruce', 'Banner', 1, 4),
+('Bruce', 'Wayne', 13, 3),
+('Wilbert', 'Jeffery', 11, 2), 
+('Erica', 'Michelle', 2, 3), 
+('Veronica', 'Archer', 4, 1), 
+('Roxanne', 'Parker', 5, 5), 
+('Billy', 'Bastion', 3, 5), 
+('Minerva', 'Jennings', 10, NULL), 
+('Albert', 'Garcia', 11, 12),
+('Mary', 'Martha', 12, 4),
+('Stephen', 'Strange', 4, 3), 
+('Vincent', 'Vermont', 5, 6),
+('Virgil', 'Abloh', 7, 5);
 
