46750c798d7eebd6a9b0b6f7e35dfb1e3e0bf2b8
blur <damienhebert710@gmail.com>
2022-05-22 23:09:18 -0500

    made title, salary, and depertment id values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,25 @@
 INSERT INTO department (department_name)
-VALUES ('Genres'), 
-('Hip hop'),
-('Country'), 
-('RnB'), 
-('Pop');
+VALUES ('Sales'), 
+('Programming'),
+('Executive'), 
+('IT'), 
+('Human Resources');
 
 
-INSERT INTO roles ()
+INSERT INTO roles (title, salary, depertment_id)
+VALUES ('Chief Executive Officer', 350000, 3),
+('Chief Financial Officer', 300000, 3)
+('Chief Operating Officer', 300000, 3)
+('IT Project Manager', 750000, 4)
+('IT Project Director', 120000, 4)
+('HR Director', 250000, 5)
+('HR Generalist', 70000, 5)
+('HR Coordinator', 65000, 5)
+('Sr.Developer', 175000, 2)
+('Jr. Developer', 125000, 2)
+('Sales Director', 400000, 1)
+('Sr. Account Executive', 250000, 1)
+('Account Executive', 175000, 1);
+
+
 
